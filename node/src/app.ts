94a25deed0db4ba25b8a29527abb17cf41f883bb
Herivelton <herivelton@pop-os.localdomain>
2021-10-19 21:49:08 -0300

    aula01-node

---
+++ node/src/app.ts
@@ -0,0 +1,43 @@
+import "dotenv/config";
+import express from "express";
+import http from "http";
+import cors from "cors";
+
+//conexão web socket entre cliente e servidor. Sem fechar conexão a todo momento
+import { Server } from "socket.io";
+
+import {router} from "./routes";
+
+const app = express();
+app.use(cors());
+
+const serverHttp = http.createServer(app);
+
+const io = new Server(serverHttp, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`Usuário conectado no socket ${socket.id}`);
+});
+
+
+app.use(express.json());
+
+app.use(router);
+
+app.get("/github", (request, response) => {
+  response.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
+  );
+});
+
+app.get("/signin/callback", (request, response) => {
+  const { code } = request.query;
+
+  return response.json(code);
+})
+
+export {serverHttp, io}
